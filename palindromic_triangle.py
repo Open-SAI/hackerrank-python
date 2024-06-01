3e4f85ec4efa36082b30e78a4bbb11bc2648b5a8
nemqueteba <nemqueteba@gmail.com>
2024-06-01 17:55:45 -0500

    Palindromic Triangle context description...

--- palindromic_triangle.py
+++ palindromic_triangle.py
@@ -1,15 +1,21 @@
-#for i in range (0, int(input())): 
-#    for n in range (0, i):
-#        print (i, end='')
-#    print()
-#    print((lambda i: i)(i))
-#    print (range (0, i))
-#print(list(map(serial:=lambda x: x*pow(10,x-1) + serial(x-1) if x>1 else x, range(1,int(input())))))
-#print(list(map(lambda x: int(x*(pow(10,x)-1)/9), range(1,int(input())))))
-#for serie in (list(map(lambda x: int(x*(pow(10,x)-1)/9), range(1,int(input()))))):print (serie)
-#for count, line in enumerate(map((serie:=lambda x: pow(10,x-1) + serie(x-1) if x>1 else x),range(1,int(input()))),1): print (count*line)
-#for count in range(1,int(input())+1):
-#    print (count,((10**count)//2),end=':')
-#    print (list(map(serie:=lambda x: pow(10,x-1)*x + serie(x-1) if x>1 else x,range(1,(count*2)-1))))
+'''
+This exercise must be print a palindromic number triangle for a entred integer number, example:
+
+If we type n = 7:
+ 7
+
+We get:
+ 1
+ 121
+ 12321
+ 1234321
+ 123454321
+ 12345654321
+ 1234567654321
+
+The difficulty appears when the program only can  contain from one to two lines of code.
+
+The use of "lambda functions" in his recursive mode, can be useful here, and there is an mathematical tip: the use of multiplication of series of numbers 1, 11, 111, 1111, 11111, ..., from the number theory. This type of multiplication has a behavior like the palindromic objective triangle.
+'''
 for count, line in enumerate(map((serie:=lambda x: pow(10,x-1) + serie(x-1) if x>1 else x),range(1,int(input())+1)),1): print (line*line)
 
