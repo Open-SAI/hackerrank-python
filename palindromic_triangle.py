86832bd21a595b79bfe02cdb50152619faa36f35
David Toro Triana <dtorot@opensai.org>
2024-06-01 19:10:55 -0500

    Updated README with the 'Palindromic Triangle' problem. Refactor the palindromic_triangle.py, it is not needed the use of the enumerate function

--- palindromic_triangle.py
+++ palindromic_triangle.py
@@ -17,5 +17,5 @@ The difficulty appears when the program only can  contain from one to two lines 
 
 The use of "lambda functions" in his recursive mode, can be useful here, and there is an mathematical tip: the use of multiplication of series of numbers 1, 11, 111, 1111, 11111, ..., from the number theory. This type of multiplication has a behavior like the palindromic objective triangle.
 '''
-for count, line in enumerate(map((serie:=lambda x: pow(10,x-1) + serie(x-1) if x>1 else x),range(1,int(input())+1)),1): print (line*line)
+for line in map((serie:=lambda x: pow(10,x-1) + serie(x-1) if x>1 else x),range(1,int(input())+1)): print (line)
 
